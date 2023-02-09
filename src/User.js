f0287c7ee0e67fd5a35e9dff59283232f3d0f581
MHP678 <muhammadpatel465@gmail.com>
2023-02-09 16:30:21 +0000

    " In process"

--- src/User.js
+++ src/User.js
@@ -1,5 +1,16 @@
 class User {
+
   // User code here
+  constructor(username, password,age){
+    this.username = username
+    this.password = password
+    this.age = age
+  }
+
+  
+
 }
+
+
 
 module.exports = User
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,43 @@
 const User = require('../src/User')
 
 // User tests here
-
 // test username
+describe('username function' , () => {
 
+    test('checks if username is correct', () => {
+        const user = new User('Max', 'password123', 18);
+        expect(user.username).toBe('Max'); 
+    })
+
+})
 // test password
+describe('password function' , () => {
 
+    test('checks if password is correct' , () => {
+        const user = new User('Max', 'password123', 18);
+        expect(user.password).toBe('password123'); 
+    })
+})
 // test age
+describe('age function' , () => {
 
+    test('checks if age is correct' , () => {
+        const user = new User('Max', 'password123', 18);
+        expect(user.age).toBe(18);         
+    })
+})
 // test login
+describe('login function' , () => {
 
+    test('checks if login is successful' , () => {
+        
+    })
+})
 // test logout
+describe('password function' , () => {
+
+    test('checks if logout is successful' , () => {
+        
+    })
+})
+
